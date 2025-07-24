c056ad222d8354f67824ee3f53eb3e7ea61f8cab
It-NatBert <n.bertrand@it-students.fr>
2025-07-24 16:03:41 +0200

    Logique d'envoie des data front -> backend

---
+++ assets/react/components/Button/ButtonPost.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ButtonPost = () => {
+    return (
+        <button className="button-new-leaf button">
+            Poster
+        </button>
+    );
+};
+
+export default ButtonPost;
--- assets/react/components/Button/ButtonRegister.tsx
+++ assets/react/components/Button/ButtonRegister.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const ButtonRegister = () => {
+type ButtonRegisterProps = {
+    pseudo: string;
+    email: string;
+    password: string;
+
+}
+
+const ButtonRegister = ({ pseudo, email, password }: ButtonRegisterProps) => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleOnClick = () => {
+        setIsLoading(true);
+        try {
+            // Nathan : 24/07/2025 Changer l'url pour l'API d'enregistrement
+            const res = fetch('/api/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ pseudo, email, password }),
+            })
+        } catch (error) {
+            console.error('Error:', error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
     return (
-        <button className="button-new-leaf button">
-            Enregistrer
+        <button className="button-new-leaf button" onClick={handleOnClick}>
+            {isLoading ? 'Chargement...' : 'Enregistrer'}
         </button>
     );
 }

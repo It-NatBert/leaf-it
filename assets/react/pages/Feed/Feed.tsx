30d9d3f8c30437df32658ce3f82eb3bbcbb2bc29
Tristan Lacombe <tlacombevulca@outlook.fr>
2025-07-28 09:55:36 +0200

    refactor(app): Ajustement mineurs sur le feed

--- assets/react/pages/Feed/Feed.tsx
+++ assets/react/pages/Feed/Feed.tsx
@@ -1,34 +1,24 @@
 import React from "react";
 import fakeNotes from "./fakeNotes";
-
-type Note = {
-  id: number;
-  user_id: number;
-  dateNote: string;
-  isOnTheTree: boolean;
-  content: string;
-  status: "posted" | "archived" | "deleted";
-};
+import {Heart} from "lucide-react";
+import {Note} from "../../types/Note";
+import NoteComponent from "../../components/Note"
 
 const Feed = () => {
-  const notes: readonly Note[] = fakeNotes;
+    const notes: readonly Note[] = fakeNotes;
 
-  return (
-    <div className="feed-container">
-        <h1 className="feed-title">Posts du jour</h1>
-        <div className="feed-posts">
-            {fakeNotes
-            .filter((note) => note.status === "posted")
-            .map((note) => (
-                <div key={note.id} className="post-card">
-                <p className="post-author">@MoniqueBellucci</p>
-                <p className="post-content">{note.content}</p>
-                <div className="post-footer">♡</div>
-                </div>
-            ))}
+    return (
+        <div className="feed-container">
+            <h1 className="feed-title">Posts du jour</h1>
+            <div className="feed-posts">
+                {fakeNotes
+                    .filter((note) => note.status === "posted")
+                    .map((note) => (
+                        <NoteComponent note={note}/>
+                    ))}
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default Feed;

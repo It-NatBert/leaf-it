c66476024f71324cda767c1c697dd31b3d62bb30
Tristan Lacombe <tlacombevulca@outlook.fr>
2025-07-30 11:45:22 +0200

    feat(app): Tweaking de la page d'édition d'un nouveau leaf

--- assets/react/pages/NewLeaf/NewLeaf.tsx
+++ assets/react/pages/NewLeaf/NewLeaf.tsx
@@ -10,30 +10,34 @@ const NewLeafPage = () => {
 
     return (
         <div className="new-leaf-page-container-mobile">
-            <h1 className="title">Nouvelle leaf</h1>
             <form>
-                <label id="message" className="label-message">
-                    <span className="labels">Que voulez-vous dire aujourd’hui ?</span>
-                    <textarea maxLength={512} placeholder={"Écrivez votre message ici..."} name={"message"}
-                              onChange={(e) => {
-                                  if (e.target.value.length >= 512) {
-                                      e.target.value = e.target.value.slice(0, 512)
+                <div className={"sub-container"}>
+                    <label id="message" className="label-message">
+                        <span className="labels">Que voulez-vous dire aujourd’hui ?</span>
+                        <textarea maxLength={512} placeholder={"Écrivez votre message ici..."} name={"message"}
+                                  onChange={(e) => {
+                                      if (e.target.value.length >= 512) {
+                                          e.target.value = e.target.value.slice(0, 512)
+                                      }
+                                      setMessage(e.target.value)
                                   }
-                                  setMessage(e.target.value)
-                              }
-                              }/>
-                </label>
-                <p className={"characters-count"}>{message.length}/512</p>
-                <div className={"flag-zone"}>
-                    {flag ?
-                        <p>Cette Leaf s’affichera sur votre arbre</p> :
-                        <p>Ajouter cette leaf à mon arbre</p>
-                    }
-                    <ButtonAddTrees flag={flag} setFlag={setFlag}/>
+                                  }/>
+                    </label>
+                    <p className={"characters-count"}>{message.length}/512</p>
+                    <div className={"flag-zone"}>
+                        {flag ?
+                            <p>Cette Leaf s’affichera sur votre arbre</p> :
+                            <p>Ajouter cette leaf à mon arbre</p>
+                        }
+                        <ButtonAddTrees flag={flag} setFlag={setFlag}/>
+                    </div>
                 </div>
             </form>
 
-            <ButtonPublish flag={flag} date_note={new Date(Date.now())} id_user={3} content={message}/>
+            <div className={"decision-zone"}>
+                <a href={"/"} className={"cancel-button"}>Annuler</a>
+                <ButtonPublish flag={flag} date_note={new Date(Date.now())} id_user={3} content={message}/>
+            </div>
         </div>
     );
 };
